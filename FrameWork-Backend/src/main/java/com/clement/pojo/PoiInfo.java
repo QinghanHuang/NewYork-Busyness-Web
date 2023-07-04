8093b7328609dea2b673e6b0298df186c3afa0f4
Picto <yifanwu0097@gmail.com>
2023-07-04 17:25:40 +0100

    update apis for getting poi and poi_detail json

--- FrameWork-Backend/src/main/java/com/clement/pojo/PoiInfo.java
+++ FrameWork-Backend/src/main/java/com/clement/pojo/PoiInfo.java
@@ -2,19 +2,17 @@ package com.clement.pojo;
 
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 public class PoiInfo {
     private String id;
     private String name;
     private String detailedLocation;
-//    private String open;
-//    private String close;
     private OpenTime openTime;
     private String busy;
-//    private String url1;
-//    private String url2;
-//    private String url3;
-    private Img img;
+    private List<String> img = new ArrayList<>();
     private String introduction;
 
     @Data
@@ -37,31 +35,15 @@ public class PoiInfo {
         openTime.setClose(close);
     }
 
-    @Data
-    public static class Img{
-        private String url1;
-        private String url2;
-        private String url3;
-    }
-
     public void setUrl1(String url1){
-        if(img == null){
-            img = new Img();
-        }
-        img.setUrl1(url1);
+        img.add(url1);
     }
 
     public void setUrl2(String url2){
-        if(img == null){
-            img = new Img();
-        }
-        img.setUrl2(url2);
+        img.add(url2);
     }
 
     public void setUrl3(String url3){
-        if(img == null){
-            img = new Img();
-        }
-        img.setUrl3(url3);
+        img.add(url3);
     }
 }
