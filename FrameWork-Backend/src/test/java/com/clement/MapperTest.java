abcd0500b663765c5a1406e83d865de52e651922
QinghanHuang <darvish841@gmail.com>
2023-07-27 23:41:22 +0100

    deploy the first model

--- FrameWork-Backend/src/test/java/com/clement/MapperTest.java
+++ FrameWork-Backend/src/test/java/com/clement/MapperTest.java
@@ -1,6 +1,7 @@
 package com.clement;
 
 import com.clement.dao.AccountMapper;
+import com.clement.dao.PoiMapper;
 import com.clement.pojo.AccountDetail;
 import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
@@ -18,12 +19,20 @@ import java.sql.Date;
 public class MapperTest {
     @Resource
     AccountMapper accountMapper;
+
+    @Resource
+    PoiMapper poiMapper;
     @Test
     public void test(){
         AccountDetail clement = accountMapper.findAccountDetailByUsernameOrEmail("clement");
         clement.setAvatar(2);
         clement.setBirthday(Date.valueOf("2001-08-18"));
         accountMapper.UpdateAccountDetailByUsernameOrEmail(clement);
+    }
+    @Test
+    public void test1(){
+        System.out.println(poiMapper.getAreaByPid(12));
+
     }
 }
 
